29b64ae322526971fc6fa723d05837810c9c16fa
Vincent BRIAND <vincent-briand_student2022@wilder.school>
2022-08-10 15:43:17 +0200

    fix DaisyUI by Tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.jsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
+  ],
+  // eslint-disable-next-line
+  plugins: [require("daisyui")],
 };
