121f690c19ffff29a185858b5feffee7c9c36fa5
David71700 <dadeire@icloud.com>
2022-08-10 23:58:37 +0200

    add faq

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,30 @@
-import { Button, Navbar } from "react-daisyui";
+import Faq from "../components/Faq";
 
 function Home() {
+  const collapses = [
+    {
+      index: 0,
+      title: "Foire aux questions",
+      content: " comment tué un reptilien ?",
+    },
+    {
+      index: 1,
+      title: "Foire aux poubelles",
+      content: " attribute is necessary to make the div focusable",
+    },
+    {
+      index: 2,
+      title: "Foire aux culs",
+      content: " attribute is necessary to make the div focusable",
+    },
+  ];
+
   return (
-    <div className="flex w-full component-preview p-4 items-center justify-center gap-2 font-sans bg-white">
-      <Navbar>
-        <Button className="text-xl normal-case" color="ghost">
-          daisyUI
-        </Button>
-      </Navbar>
+    <div>
+      {collapses.map((collapse) => (
+        <Faq collapse={collapse} key={collapse.index} />
+      ))}
     </div>
   );
 }
-
 export default Home;
