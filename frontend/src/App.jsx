85f7d6c36effd2066cf6fcc4bc1dcd29cf4fc257
Vincent BRIAND <vincent-briand_student2022@wilder.school>
2022-08-12 05:28:51 +0200

    merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import { useRoutes } from "react-router-dom";
 
-<<<<<<< HEAD
 import Inventory from "@pages/Inventory";
-=======
 import SecretPage from "./components/SecretPage";
->>>>>>> dev
 import LayerBasic from "./layers/LayerBasic";
 import LayerHome from "./layers/LayerHome";
 
@@ -25,11 +22,8 @@ function App() {
       children: [
         { path: "/shop", element: <Shop /> },
         { path: "/training", element: <Training /> },
-<<<<<<< HEAD
         { path: "/inventory", element: <Inventory /> },
-=======
         { path: "/SecretPage", element: <SecretPage /> },
->>>>>>> dev
       ],
     },
   ]);
